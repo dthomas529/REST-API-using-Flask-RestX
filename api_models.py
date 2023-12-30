7000e69c9eecff5d12f3eba37e7272d8c74ad03f
Dawn F. Thomas <dthomas529@gmail.com>
2023-12-30 18:01:30 -0500

    Update api_models.py
    Creates a dictionary for the model values

---
+++ api_models.py
@@ -0,0 +1,9 @@
+from flask_restx import fields
+from extensions import api
+
+course.model = api.model('Course', {
+"id": fields.Integer
+"course": fields.String
+#students:
+})
+                         
