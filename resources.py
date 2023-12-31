8cdd884e3518ae2424fe52c844f61c11aeb68700
Dawn F. Thomas <dthomas529@gmail.com>
2023-12-31 17:59:50 -0500

    Update resources.py
    Additions made to file

--- resources.py
+++ resources.py
@@ -1,6 +1,6 @@
 from flask-restx import Resource, Namespace
-
-from .api_models import course_models, student_models
+from .extenions import db
+from .api_models import course_model, student_model, course_input_model, student_input_model
 from .models import Course
 
 ns = Namespace("api")
@@ -11,13 +11,54 @@ class Hello(Resource):
     return {"hello": "rest-x"}
     
 @ns.route("/courses")
-class CourseAPI(Resource):
+class CourseListAPI(Resource):
   @ns.marshal_list_with(course_models)
   def get(self):
     return Course.query.all()
 
+@ns.expect(course_input_model)
+@ns.marshal_with(course_models)
+def post(self):
+  print(ns.payload)
+  course = Course(name=ns.payload["name"])
+  db.session.add(course)
+  db.session.commit()
+  return course, 201
+
+@ns.route("/courses/<int:id>")
+class CourseAPI(Resource):
+  @ns.marshal_with(course_model)
+  def get(self, id):
+    return Course.query.get(id)
+  
+  @ns.expect(course_input_model)
+  @ns.marshal_with(course_model)
+  def put(self, id):
+    course = Course.query.get(id)
+    return course
+
 @ns.route("/students")
-class StudentAPI(Resource):
-  @ns.marshal_list_with(_models)
+class StudentListAPI(Resource):
+  @ns.marshal_list_with(student_model)
   def get(self):
     return Student.query.all()
+
+@ns.expect(student_input_model)
+@ns.marshal_with(student_model)
+def post(self):
+  student = Student(name=ns.payload["name"], course_id=ns.payload["course_id"])
+  db.session.add(student)
+  db.session.commit()
+  return student, 201
+
+@ns.route("/students/<int:id>")
+class StudentAPI(Resource):
+  @ns.marshal_with(student_model)
+  def get(self, id):
+    return Student.query.get(id)
+
+  @ns.expect(student_input_model)
+  @ns.marshal_with(student_model)
+  def put(self, id):
+    student = Student.query.get(id)
+    return student
