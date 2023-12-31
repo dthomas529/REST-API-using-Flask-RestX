9af217bf7d0e2732786ce560b194e2940b9828d0
Dawn F. Thomas <dthomas529@gmail.com>
2023-12-31 18:05:51 -0500

    Update api_models.py
    Additional models added

--- api_models.py
+++ api_models.py
@@ -1,9 +1,22 @@
 from flask_restx import fields
 from extensions import api
 
-course.model = api.model('Course', {
-"id": fields.Integer
-"course": fields.String
-#students:
+students_model = api.model('Student', {
+"id": fields.Integer,
+"name": fields.String,
+"course": fields.nested(course.model)
 })
-                         
+
+course_model = api.model('Course', {
+"id": fields.Integer,
+"name": fields.String,
+  "students": fields.List(fields.nested(student_model))
+
+course_input_model = api.model("CourseInput", {
+  "name": fields.String
+})
+
+student_input_model = api.model("StudentInput", {
+  "name": fields.String,
+  "course_id": fields.Integar
+})
